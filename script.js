19a6bfb3de5f5012b1e330e20e84aaafd246fd92
emreeeee456789 <emreu564@icloud.com>
2025-07-20 19:14:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function feed() {
+  document.getElementById('response').innerText = "Mino afiyetle yemeğini yedi 😋";
+}
+function pet() {
+  document.getElementById('response').innerText = "Mino mırıldıyor... Seni çok seviyor ❤️";
+}
+function play() {
+  document.getElementById('response').innerText = "Mino zıplıyor, hopluyor! Çok mutlu 🎉";
+}
