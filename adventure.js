89229c94244c4a713ac378b23c3058a8fa4b9a81
emreeeee456789 <emreu564@icloud.com>
2025-07-26 19:54:16 +0300

    Update adventure.js

--- adventure.js
+++ adventure.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabaseUrl = 'https://XXX.supabase.co';
-const supabaseKey = 'public-anon-key';
+const supabaseUrl = 'https://ghtmaqmodwrumxpvgkjl.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodG1hcW1vZHdydW14cHZna2psIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1NDYzNzgsImV4cCI6MjA2OTEyMjM3OH0.5pFJRGOmA-vlJ0rNcdujGsxx-KLJAYyveiTuuA0QRIU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function miniMaceraBaslat(ulke) {
